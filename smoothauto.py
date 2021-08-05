98fc700c633bd998f832f1410fe4c2abe04fcb25
Mohammed Zenudeen Shehbaz <84646462+mohammedz00@users.noreply.github.com>
2021-08-05 17:12:37 +0100

    Add files via upload

---
+++ smoothauto.py
@@ -0,0 +1,41 @@
+bl_info = {
+    "name": "Smooth Auto",
+    "author": "Zenudeen Shehbaz",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+
+}
+
+
+
+import bpy
+
+#bpy.ops.object.shade_smooth()
+#bpy.context.object.data.use_auto_smooth = True
+
+
+class SmoothAuto(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.smooth_auto"
+    bl_label = "Smooth Auto"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        bpy.ops.object.shade_smooth()
+        bpy.context.object.data.use_auto_smooth = True
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(SmoothAuto)
+
+
+def unregister():
+    bpy.utils.unregister_class(SmoothAuto)
+
+
+if __name__ == "__main__":
+    register()
+
